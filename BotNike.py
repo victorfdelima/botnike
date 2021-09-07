4542365254e1c5b05155b7254c7a9671dace7c90
Victor de Lima <vittinferreira@gmail.com>
2021-09-07 02:31:24 -0300

    atualizacao

--- BotNike.py
+++ BotNike.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -71,6 +68,7 @@ time.sleep(2);
 driver.find_element_by_css_selector('#btn-comprar').click()
 time.sleep(1);
 print(f"{botName} Diz:  Pronto! Agora irei autenticar via SMS..")
+# Adiciona humanamente um numero de celular para receber SMS
 driver.find_element_by_name('CelularCliente').send_keys('3')
 time.sleep(2)
 driver.find_element_by_name('CelularCliente').send_keys('1')
@@ -96,7 +94,7 @@ print(f"{botName} Diz: Autenticando..")
 time.sleep(0.2)
 print(f"{botName} Diz: Quase la..")
 time.sleep(20)
-
+# Método para realizar contagem em segundos para inputar código enviado para o celular
 for c in range(20, -1, -1):
     c = c - 0
     time.sleep(1)
@@ -112,7 +110,11 @@ time.sleep(1.5)
 driver.find_element_by_id('seguir-pagamento').click()
 time.sleep(2)
 driver.find_element_by_css_selector('button.undefined:nth-child(1)').click()
+print(f"{botName} Diz:  Addicionando dados do cartão agora...")
 time.sleep(2)
+
+# Inputa os dados do cartão para pagamento
+
 driver.find_element_by_id('ccard-number').click()
 time.sleep(2)
 driver.find_element_by_id('ccard-number').send_keys('4716 6217 2264 7088', Keys.TAB)
@@ -121,6 +123,7 @@ driver.find_element_by_id('ccard-owner').click()
 time.sleep(1)
 driver.find_element_by_id('ccard-owner').send_keys('Jefferson Lucas Gomes', Keys.TAB)
 time.sleep(1)
+# Adiciona o CPF
 driver.find_element_by_id('ccard-document').send_keys('5', Keys.TAB)
 time.sleep(0.3)
 driver.find_element_by_id('ccard-document').send_keys('5', Keys.TAB)
@@ -143,6 +146,8 @@ driver.find_element_by_id('ccard-document').send_keys('5', Keys.TAB)
 time.sleep(0.3)
 driver.find_element_by_id('ccard-document').send_keys('5', Keys.TAB)
 time.sleep(0.5)
+# Coloca a data de validade do cartão
+print(f"{botName} Diz:  Escolhendo a data exata do cartão")
 driver.find_element_by_css_selector('#exp-month').click()
 time.sleep(0.5)
 driver.find_element_by_css_selector('#exp-month > option:nth-child(4)').click()
@@ -151,26 +156,14 @@ driver.find_element_by_css_selector('#exp-year').click()
 time.sleep(0.5)
 driver.find_element_by_css_selector('#exp-year > option:nth-child(9)').click()
 time.sleep(1)
+# Aceita política de privacidade
+print(f"{botName} Diz:  Pronto, agora aceitando os termos e políticas...")
 driver.find_element_by_css_selector('#security-code').send_keys('892')
 time.sleep(0.6)
 driver.find_element_by_css_selector('#linkTrocasCancelamentos').click()
 time.sleep(1)
 driver.find_element_by_css_selector('.false > button:nth-child(1)').click()
 time.sleep(1)
+print(f"{botName} Diz:  Confirmando o pagamento...")
+# Clica no botão de pagar
 driver.find_element_by_css_selector('#confirmar-pagamento').click()
-
-#Inputa o numero do telefone e aguarda 4 segundos para clicar
-
-
-
-#driver.find_element_by_xpath('/html/body/main/div[1]/div[8]/div[2]/div[5]/a').click()
-#time.sleep(1)
-
-#bot da nike entra no site (done)
-#faz login (done)
-#coloca um produto no carrinho (done)
-#Deixa o checkout pronto com o produto()
-#Na aba nova retira o produto antigo()
-#Pede SMS, autentica o SMS()
-#coloca no carrinho o tenis
-#Faz o checkout do SKU adicionado no Config.json no checkout da aba anterior.
